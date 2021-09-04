8f2e3c4c27477a217fa6f3adccd8d4fff4396873
Rabindra Yadav <rabindrayadav9596@gmail.com>
2021-09-04 13:51:32 -0500

    refactor

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+
+import React from "react";
+import {StreamChat} from "stream-chat";
+import {Chat} from "stream-chat-react";
+import Cookies from "universal-cookie";
+
+const apiKey = "berkf4tvewgk";
+
+
+const App =() =>{
+    return(
+        <div>
+            Chat app;
+        </div>
+    )
+}
+
+export default App;
